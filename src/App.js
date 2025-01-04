9422c19c1d42554eca99f92989017f3c4047f120
Xander <xander.cage.botha@gmail.com>
2025-01-04 16:25:51 +0000

    add testing live updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Xander
+          Edit <code>src/App.js</code> and save to reload. Xander. testing live updates
         </p>
         <a
           className="App-link"
